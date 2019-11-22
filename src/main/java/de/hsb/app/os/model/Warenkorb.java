2b781fbd0847ee2046ed20da174e9fd902aa9f72
RPDracken <P.Zacheja@gmx.de>
2019-11-22 14:40:16 +0100

    Added mthodes for warenkorb.java

--- src/main/java/de/hsb/app/os/model/Warenkorb.java
+++ src/main/java/de/hsb/app/os/model/Warenkorb.java
@@ -1,5 +1,8 @@
 package de.hsb.app.os.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -7,6 +10,27 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class Warenkorb {
 	private String artikel;
+	private List<Produkt> article;
+	
+	public Warenkorb() {
+		article = new ArrayList<>();
+	}
+	
+	public void addProdukt(Produkt p) {
+		article.add(new Produkt(p.getMarke(), p.getTitel(), p.getBeschreibung(),
+									p.getDuftnote(), p.getPreis(), p.getWaehrungtyp(),
+									p.getMenge(),p.getMengentyp(),p.getKategorie()));
+	}
+	public void deleteProdukt(int index) {
+		article.remove(index);
+	}
+	public List<Produkt> getArticle() {
+		return article;
+	}
+
+	public void setArticle(List<Produkt> article) {
+		this.article = article;
+	}
 
 	public String getArtikel() {
 		return artikel;
